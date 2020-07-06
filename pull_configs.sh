3b99ec1882ede881281998bca7b6cc53007accde
k0va1 <al3xander.koval@gmail.com>
2020-07-06 21:37:19 +0300

    Update i3 & polybar configs

--- pull_configs.sh
+++ pull_configs.sh
@@ -3,3 +3,12 @@
 # copy vim configs
 cp -nf $HOME/.config/nvim/init.vim .config/nvim/
 cp -nfr $HOME/.config/nvim/UltiSnips .config/nvim/
+
+# copy polybar configs
+cp -nTfr $HOME/.config/polybar .config/polybar/
+
+# copy i3 configs
+cp -nTfr $HOME/.config/i3 .config/i3/
+
+# copy scripts
+cp -nTfr $HOME/.local/bin .local/bin/

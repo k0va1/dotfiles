52efcee8d91ff104594281d1913a32271efc0899
k0va1 <al3xander.koval@gmail.com>
2020-07-22 22:30:16 +0300

    overwrite configs

--- pull_configs.sh
+++ pull_configs.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 # copy vim configs
-cp -nf $HOME/.config/nvim/init.vim .config/nvim/
-cp -nfr $HOME/.config/nvim/UltiSnips .config/nvim/
+yes | cp -rf $HOME/.config/nvim/init.vim .config/nvim/
+yes | cp -rf $HOME/.config/nvim/UltiSnips .config/nvim/
 
 # copy polybar configs
-cp -nTfr $HOME/.config/polybar .config/polybar/
+yes | cp -Tfr $HOME/.config/polybar .config/polybar/
 
 # copy i3 configs
-cp -nTfr $HOME/.config/i3 .config/i3/
+yes | cp -Tfr $HOME/.config/i3 .config/i3/
 
 # copy scripts
-cp -nTfr $HOME/.local/bin .local/bin/
+yes | cp -Tfr $HOME/.local/bin .local/bin/

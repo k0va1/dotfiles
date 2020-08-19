a73c0696d10c24ed59c2dfa4e0c64a8242570f84
k0va1 <al3xander.koval@gmail.com>
2020-08-19 23:58:05 +0300

    allow to restore configs

---
+++ restore_configs.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# copy vim configs
+yes | cp -Trf .config/nvim $HOME/.config/nvim/
+
+# alacritty
+yes | cp -Trf .config/alacritty $HOME/.config/alacritty/
+
+# i3
+yes | cp -Trf .config/i3 $HOME/.config/i3/
+
+# polybar
+yes | cp -Trf .config/polybar $HOME/.config/polybar/
+
+# alacritty
+yes | cp -Trf .config/alacritty $HOME/.config/alacritty/
+
+# alacritty
+yes | cp -Trf .config/picom $HOME/.config/picom/
+
+# scripts
+yes | cp -Trf .local/bin $HOME/.local/bin/
